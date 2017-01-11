a189450ea86658316706f168b1a27be9bb6f09d5
Camilo Andrés Rodríguez Garzón <crodri56@eafit.edu.co>
2017-01-10 21:27:24 -0500

    Remove librarys platforms

--- platforms/ios/build/ScrumPoker.build/Debug-iphonesimulator/ScrumPoker.build/Objects-normal/x86_64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/main.m \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/Minerva/Desktop/scrumPoker/ScrumPoker/platforms/ios/internal/TNSDebugging.h
